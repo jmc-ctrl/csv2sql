0ed9a86b8be19e7c5101019f456f6a06aa953768
jmc-ctrl <adamlamont1477@gmail.com>
2026-01-23 19:24:08 +0000

    Created skeleton of a class for the generated insertt statement.

--- main.py
+++ main.py
@@ -11,3 +11,27 @@ def open_file(file_name):
             file_data.append(rows) # Iterating through each row and appending it to the list.
         return file_data # Returns data and closes file due to with.
     return None # None is returned for an invalid file type.
+
+class InsertStatement:
+    string(table_name)
+    string(entities) 
+    list(data) 
+
+    def __init__(self, table_name, entities, data):
+        self.table_name = table_name
+        self.entities = entities
+        self.data = format_values(data)
+
+    def format_values(data):
+        modified_data = []
+        for items in data:
+            if items == data[-1]:
+                modified_data.append(f"({items})") # No , allowed for last values.
+            else:
+                modified_data.append(f"({items}),\n") # Otherwise allow the , for multiple inserts.
+
+
+        data = modified_data # Overwriting original data with modified data.
+    def create_statement(self):
+        statement = f"INSERT INTO {table_name} {entities} VALUES {data};"
+        return statement
