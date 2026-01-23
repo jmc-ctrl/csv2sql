99964e06dad5949268b90c204351358b2dbed420
jmc-ctrl <adamlamont1477@gmail.com>
2026-01-23 19:06:16 +0000

    Final attempt at C version.

--- main.c
+++ main.c
@@ -2,21 +2,19 @@
 
 // Reads file and saves it as a char array in memory.
 char readFile (char filename[]){
-    FILE *filepointer; // Pointer to file itself.
+    FILE *filepointer = fopen(filename, "r"); // Open csv file for reading.
 
     // Error handling for invalid file
     if (filepointer == NULL){
         return 1; // Return 1 for error.
     }
 
-    filepointer = fopen(filename, "r"); // Open csv file for reading.
+    
 
     char fileData[3000];
-    fgets(fileData, 3000, filepointer);
-
-    /*while(fgets(fileData, 3000, filepointer)){
-        
-    }*/
+    fgets(fileData, sizeof(fileData), filepointer); // sizeof function tells C what the size of the array is.
+    
+    fclose(filepointer); // Closes file.
 
     return *fileData;
 }
